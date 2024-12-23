11369ac865ab57ec1f7a82fbcf7239976afe9e94
neon1028 <johnniehunt28@gmail.com>
2024-12-23 08:46:08 -0600

    Animation Overhaul mostly done

--- assets/Scripts/Actions/SubActions/SubAct_AnimationInTo.gd
+++ assets/Scripts/Actions/SubActions/SubAct_AnimationInTo.gd
@@ -24,26 +24,28 @@ func get_prop_enum_values(prop_key:String)->Array:
 
 func do_thing(parent_action:BaseAction, subaction_data:Dictionary, metadata:QueExecutionData,
 				game_state:GameStateData, actor:BaseActor)->bool:
-	var animation:String = subaction_data.get('Animation', "")
-	if animation == "":
-		return true
-	if animation == "WEAPON_DEFAULT":
-		var primary_weapon = actor.equipment.get_primary_weapon()
-		if primary_weapon:
-			animation = primary_weapon.get_load_val("WeaponAnimation", null)
-	var animation_speed = 1.0
-	if subaction_data.keys().has("AnimationSpeed"):
-		animation_speed = subaction_data.get("AnimationSpeed", 1.0)
-	print("SubAct Animation: " + animation)
-	var actor_node:ActorNode = CombatRootControl.Instance.MapController.actor_nodes.get(actor.Id)
-	if actor_node and animation:
-		if animation.begins_with("move_"):
-			var actor_pos = game_state.MapState.get_actor_pos(actor)
-			var dest_pos =  MoveHandler.relative_pos_to_real(actor_pos, parent_action.PreviewMoveOffset)
-			actor_node.set_move_destination(dest_pos, 
-				CombatRootControl.QueController.FRAMES_PER_ACTION-(CombatRootControl.QueController.sub_action_index), 
-				true)
-			#actor_node.start_walk_animation(animation, animation_speed)
-		else:
-			actor_node.start_weapon_animation(animation, animation_speed)
+	printerr("DEORECIATED: SubAct_AnimationInTo called for page '%s'" % [parent_action.ActionKey])
 	return BaseSubAction.Success
+	#var animation:String = subaction_data.get('Animation', "")
+	#if animation == "":
+		#return true
+	#if animation == "WEAPON_DEFAULT":
+		#var primary_weapon = actor.equipment.get_primary_weapon()
+		#if primary_weapon:
+			#animation = primary_weapon.get_load_val("WeaponAnimation", null)
+	#var animation_speed = 1.0
+	#if subaction_data.keys().has("AnimationSpeed"):
+		#animation_speed = subaction_data.get("AnimationSpeed", 1.0)
+	#print("SubAct Animation: " + animation)
+	#var actor_node:ActorNode = CombatRootControl.Instance.MapController.actor_nodes.get(actor.Id)
+	#if actor_node and animation:
+		#if animation.begins_with("move_"):
+			#var actor_pos = game_state.MapState.get_actor_pos(actor)
+			#var dest_pos =  MoveHandler.relative_pos_to_real(actor_pos, parent_action.PreviewMoveOffset)
+			#actor_node.set_move_destination(dest_pos, 
+				#CombatRootControl.QueController.FRAMES_PER_ACTION-(CombatRootControl.QueController.sub_action_index), 
+				#true)
+			##actor_node.start_walk_animation(animation, animation_speed)
+		#else:
+			#actor_node.start_weapon_animation(animation, animation_speed)
+	#return BaseSubAction.Success
